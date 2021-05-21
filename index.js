1d2a6b8e30953e97df7b7fb8263470efd0ccdb5e
Godswill <godswilll@analogrepublic.com>
2021-05-21 09:28:22 +0100

    set up redux

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
  * @format
  */
-
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
 import { name as appName } from './app.json';
+import store from './store';
+const Main = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
